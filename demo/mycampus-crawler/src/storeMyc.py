941543cd729ec199a16bd3108bfb23a9f39fa323
kenpu <kenpu@csc3.(none)>
2010-10-12 06:03:44 -0400

    fetch and store now fully functional, and support sqlite and postgres

--- demo/mycampus-crawler/src/storeMyc.py
+++ demo/mycampus-crawler/src/storeMyc.py
@@ -7,7 +7,30 @@ import json
 import re
 from datetime import datetime
 
-engn = create_engine('sqlite:///myc.sq3')
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option('--engine', dest='engine')
+parser.add_option('--database', dest='dbname', default='uoit')
+
+(opt, args) = parser.parse_args()
+
+if not (opt.dbname and opt.engine):
+  parser.print_help()
+  sys.exit()
+else:
+  if opt.engine.startswith('postgres'):
+    url = 'postgres://ken_pu:csci3030u@localhost/%s' % opt.dbname
+  elif opt.engine.startswith('sqlite'):
+    url = 'sqlite:///%s.sq3' % opt.dbname
+  else:
+    print 'Unknown engine'
+    sys.exit(0)
+
+  print 'Using "%s" database "%s"' % (opt.engine, opt.dbname)
+  print "url=%s" % url
+
+engn = create_engine(url)
 
 Base = declarative_base()
 
@@ -33,7 +56,6 @@ class Section(Base):
   sec_code = Column(String)
   sec_number = Column(String)
   year = Column(String)
-  title = Column(String)
   course = Column(String, ForeignKey('courses.code'))
 
 class Instructor(Base):
@@ -41,8 +63,7 @@ class Instructor(Base):
   __tablename__ = 'instructors'
 
   id = Column(Integer, primary_key=True)
-  lastname = Column(String)
-  firstname = Column(String)
+  name = Column(String)
 
 class Schedule(Base):
 
@@ -57,7 +78,7 @@ class Schedule(Base):
   hour_end = Column(Integer)
   min_start = Column(Integer)
   min_end = Column(Integer)
-  schedtype2 = Column(String)
+  classtype = Column(String)
   location = Column(String)
   section_id = Column(Integer, ForeignKey('sections.id'))
 
@@ -95,15 +116,36 @@ def parse_credits(line):
   else:
     return None
 
-def parse_registration(line):
+def parse_daterange(line):
   """
   jun 28, 2010 to jan 21, 2011
+  or
+  jan 20, 2011 - apr 14, 2011
   """
 
-  return map(
-    lambda x: datetime.strptime(x, "%b %d, %Y") \
-    if x else None,
-             line.split(" to "))
+  if ' to ' in line:
+    return map(
+      lambda x: datetime.strptime(x, "%b %d, %Y") \
+      if x else None,
+               line.split(" to "))
+  else:
+    return map(
+      lambda x: datetime.strptime(x, "%b %d, %Y") \
+      if x else None,
+               line.split(" - "))
+
+def parse_timerange(line):
+  """ 3:40 pm - 5:00 pm 
+  or TBA
+  """
+  if not line or not line.strip() or 'TBA' in line: 
+    return None, None, None, None
+  
+  start, end = map(
+        lambda x: datetime.strptime(x, "%I:%M %p"),
+        line.strip().split(' - '))
+
+  return start.hour, start.minute, end.hour, end.minute
 
 def parse_semester(line):
   """
@@ -125,20 +167,53 @@ def store_section(section_js):
   sec.capacity = int(section_js['capacity'])
   sec.credits = parse_credits(section_js.get('credits'))
   sec.levels = section_js['levels']
-  sec.registration_start, sec.registration_end = parse_registration(
+  sec.registration_start, sec.registration_end = parse_daterange(
     section_js['registration'])
   sec.semester, sec.year = parse_semester(section_js['semester'])
   sec.course = course.code
 
   if sql.query(Course).filter(Course.code == course.code).count() == 0:
     sql.add(course)
+    sql.commit()
+
   sql.add(sec)
 
   sql.commit()
+
+  if section_js.get('schedule_list'):
+    for sch_js in section_js.get('schedule_list'):
+      sch = Schedule()
+      sch.date_start, sch.date_end = parse_daterange(sch_js['daterange'])
+      sch.day = sch_js['days']
+      sch.schedtype = sch_js['scheduletype']
+      sch.hour_start, sch.min_start, \
+      sch.hour_end, sch.min_end = parse_timerange(sch_js['time'])
+      sch.classtype = sch_js['type']
+      sch.location = sch_js.get('where')
+      sch.section_id = sec.id
+      sql.add(sch)
+      sql.commit()
+      for inst_js in sch_js.get('instructor_list', []):
+        name = inst_js.get('name')
+        position = inst_js.get('position')
+        if name and not 'TBA' in name:
+          q = sql.query(Instructor).filter(Instructor.name == name).all()
+          if not q:
+            instructor = Instructor()
+            instructor.name = name
+            sql.add(instructor)
+            sql.commit()
+          else:
+            instructor = q[0]
+          teaches = Teaches()
+          teaches.instructor_id = instructor.id
+          teaches.schedule_id = sch.id
+          sql.add(teaches)
+          sql.commit()
   
 def load(lines):
   for (i, line) in enumerate(lines):
-    print "line: %d, index: %d" % (i+1, i)
+    print "line: %d" % (i+1)
     sec_js = json.loads(line)
     store_section(sec_js)
 
