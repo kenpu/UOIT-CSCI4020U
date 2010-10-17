4ce6f00264d7dd1d604cdafe2ca43762ff6da1d8
kenpu <kenpu@csc3.(none)>
2010-10-17 05:20:16 -0400

    intro

---
+++ 01-intro-lex/test_basic.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+char *yytext;
+int yyleng;
+int yylex(void);
+
+int main(int argc, char **argv) {
+    int token_type;
+    while(token_type = yylex()) {
+        printf("<%d, \"%s\">\n", token_type, yytext);
+    }
+}
